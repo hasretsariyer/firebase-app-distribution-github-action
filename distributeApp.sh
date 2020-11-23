57cc584c8732f0036592ea77d701f323875c04db
hasretsariyer <hasretsariyer@gmail.com>
2020-11-23 23:50:27 +0300

    Update distributeApp.sh

--- distributeApp.sh
+++ distributeApp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-if [[ -z ${RELEASE_NOTES} ]]; then
-    RELEASE_NOTES="$(git log -1 --abbrev-commit --pretty=oneline)"
+if [[ -z ${INPUT_RELEASE_NOTES} ]]; then
+    INPUT_RELEASE_NOTES="$(git log -1 --abbrev-commit --pretty=oneline)"
 fi
 
-firebase appdistribution:distribute "$APP_FILE" --app \
-    "$APP_ID" --token "$FIREBASE_TOKEN" --groups "$TESTER_GROUPS" \
-    --release-notes "$RELEASE_NOTES"
+firebase appdistribution:distribute "$INPUT_APP_FILE" --app \
+    "$INPUT_APP_ID" --token "$INPUT_FIREBASE_TOKEN" --groups "$INPUT_TESTER_GROUPS" \
+    --release-notes "$INPUT_RELEASE_NOTES"
